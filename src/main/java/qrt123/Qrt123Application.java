2165b4a8de2d0949d2df2304d2138a5032b1085f
Matt Cadorette <mcadorette@gmail.com>
2018-11-02 12:22:54 -0400

    Initial commit from Atomist

---
+++ src/main/java/qrt123/Qrt123Application.java
@@ -0,0 +1,12 @@
+package qrt123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qrt123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qrt123Application.class, args);
+	}
+}
